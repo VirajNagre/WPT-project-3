c9e819a2acbb2f06e293d94322c6315729d43aa6
jitendra <choudharyjitendra125@gmail.com>
2024-05-20 23:33:05 +0530

    footer code

--- frontend/esportsapp/src/App.js
+++ frontend/esportsapp/src/App.js
@@ -10,12 +10,14 @@ import EventInfo from './components/EventInfo.js'
 import { AuthProvider } from './components/ContextAPI/authContext.js';
 import toast, { Toaster } from 'react-hot-toast';
 
+
 import { Container } from "react-bootstrap";
 // <<<<<<< HEAD
 import { MyProfile } from "./components/MyProfile.js";
 import { UpdateInfo } from "./components/UpdateInfo.js";
 // =======
 import { PrivateRoute } from "./components/Routing/PrivateRoute.js";
+import { Footer } from "./components/Footer.js";
 // >>>>>>> 7bd3d0f568ec1024da75611e1717c578588afef3
 function App() {
   return (
@@ -39,12 +41,15 @@ function App() {
 
               <Route path="/event/create-new" element={<NewEventPage />} />
             </Route>
-
+            
 \          </Routes>
+         
         </Router>
+        
       </AuthProvider>
-
+      <Footer/>
     </>
+
   );
 }
 
--- frontend/esportsapp/src/components/Footer.js
+++ frontend/esportsapp/src/components/Footer.js
@@ -1,16 +1,38 @@
-
+import React from "react";
 export const Footer=()=>{
     return(
-        <div>
-        <div
-            style={{
-                minHeight: "400px",
-                color: "green",
-            }}
-        >
-            <h1>Esports</h1>
+        <div className="main-footer bg-dark text-white py-4 mt-3 mb-0" >
+             <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6">
+                        <h5>ContactUs</h5>
+                        <ul className="list-unstyled">
+                            <li>+91 8291343998/+91 9594514427</li>
+                            <li>abc123@gmail.com</li>
+                            <li>CDAC Mumbai Kharghar 400614</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3 col-sm-6">
+                        <h5>Our Commitments</h5>
+                        
+                    </div>
+                    <div className="col-md-3 col-sm-6">
+                        <h5>Careers</h5>
+                       
+                    </div>
+                    <div className="col-md-3 col-sm-6">
+                        <h5>Help</h5>
+                       
+                    </div>
+                    
+                </div>
+                
+                <div className="footer-bottom">
+                    <p className="text-xs-center">
+                        &copy;{new Date().getFullYear()} Esports App -All Rights Reserved
+                    </p>
+                </div>
+             </div>
         </div>
-        <Footer />
-    </div>
     );
 }
